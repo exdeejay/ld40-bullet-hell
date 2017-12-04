54efee73fccd4ac546414308770ce8b04951a99e
DJ <djnsmd@gmail.com>
2017-12-03 20:14:55 -0600

    Made relevant changes to fix HTML port

--- html/src/net/bmagic/ld40/client/HtmlLauncher.java
+++ html/src/net/bmagic/ld40/client/HtmlLauncher.java
@@ -3,13 +3,17 @@ package net.bmagic.ld40.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+import net.bmagic.ld40.CameraController;
 import net.bmagic.ld40.Game;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig() {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration(
+                        (int) (CameraController.CAMERA_WIDTH * 1.25),
+                        (int) (CameraController.CAMERA_HEIGHT * 1.25));
         }
 
         @Override
